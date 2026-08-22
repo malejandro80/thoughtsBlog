44136be02392b9a7e7b5965ffe2cca64172e9dd7
Miguel Bastidas <m.alejandro_80@hotmail.com>
2026-08-22 17:03:19 -0500

    chore: scaffold inicial de Astro con sitemap

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://thoughts.miguelintech.com',
+  integrations: [sitemap()],
+});
