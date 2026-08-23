62edcdd4a11d515172e4820cbf6c0029e336a20e
Miguel Bastidas <m.alejandro_80@hotmail.com>
2026-08-22 19:13:54 -0500

    build: fuerza CSS inline en el build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://thoughts.miguelintech.com',
   integrations: [sitemap()],
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
